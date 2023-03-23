54027e281647b8c354e6ca61e137fc7c235c422b
dazhwu <63244649+dazhwu@users.noreply.github.com>
2023-03-23 08:54:59 -0400

    Update test.R

--- test.R
+++ test.R
@@ -1,26 +1,29 @@
 library(panelvar)
 data(abdata)
-p1 <-pvargmm(
-  dependent_vars = c("n","w","k"),
+p1 <- pvargmm(
+  dependent_vars = c("n", "w", "k"),
   lags = 2,
-  
-  #exog_vars=c("k"),
+
+  # exog_vars=c("k"),
   transformation = "fd",
   data = abdata,
   panel_identifier = c("id", "year"),
   steps = c("twostep"),
   system_instruments = TRUE,
-  #max_instr_dependent_vars = 3,
-  #max_instr_predet_vars = 3,
-  #min_instr_dependent_vars = 1L,
-  #min_instr_predet_vars = 1L,
+  # max_instr_dependent_vars = 3,
+  # max_instr_predet_vars = 3,
+  # min_instr_dependent_vars = 1L,
+  # min_instr_predet_vars = 1L,
   collapse = FALSE,
   progressbar = FALSE
 )
 summary(p1)
 
-girf(p1, n.ahead = 8, ma_approx_steps= 8)
-ex1_dahlberg_data_bs <- bootstrap_irf(p1, typeof_irf = c("GIRF"),
-                                      n.ahead = 8,
-                                      nof_Nstar_draws = 200,
-                                      confidence.band = 0.95)
+girf(p1, n.ahead = 8, ma_approx_steps = 8)
+ex1_dahlberg_data_bs <- bootstrap_irf(p1,
+  typeof_irf = c("GIRF"),
+  n.ahead = 8,
+  nof_Nstar_draws = 200,
+  confidence.band = 0.95
+)
+print(ex1_dahlberg_data_bs)
